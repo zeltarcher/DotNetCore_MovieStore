bf4ab8ac3633ba7e04354396837055d709cf1f0a
Quang_Truong <TRUONGCHIQUANG@GMAIL.COM>
2021-08-16 23:09:50 -0500

    update

--- MovieStore/ApplicationCore/ServiceInterfaces/IMovieService.cs
+++ MovieStore/ApplicationCore/ServiceInterfaces/IMovieService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.ServiceInterfaces
 {
-    class IMovieService
+    public interface IMovieService
     {
+        Task<List<MovieCardResponseModel>> GetTopRevenueMovies();
+
+        //Task<MovieDetailsResponseModel> GetMovieDetails(int id);
     }
 }
