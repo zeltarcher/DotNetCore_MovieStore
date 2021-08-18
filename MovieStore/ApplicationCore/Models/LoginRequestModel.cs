b6b972d75c0b2f180e681e21b68c1a39ee0b2285
Quang_Truong <TRUONGCHIQUANG@GMAIL.COM>
2021-08-17 20:41:15 -0500

    update Login Model

--- MovieStore/ApplicationCore/Models/LoginRequestModel.cs
+++ MovieStore/ApplicationCore/Models/LoginRequestModel.cs
@@ -8,5 +8,8 @@ namespace ApplicationCore.Models
 {
     public class LoginRequestModel
     {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
     }
 }
---
+++ MovieStore/ApplicationCore/ServiceInterfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.ServiceInterfaces
+{
+    public interface IUserService
+    {
+        Task<bool> RegisterUser(UserRegisterRequestModel model);
+        Task<bool> Login(LoginRequestModel model);
+    }
+}
